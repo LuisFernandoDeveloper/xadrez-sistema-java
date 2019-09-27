3281e50be25e5379124191432489cbcd024afc74
Luis Fernando <luifmoreira@gmail.com>
2019-09-27 12:29:28 -0300

    camada de xadrez e imprimir o tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,14 @@
 package application;
 
-import boardgame.Posicao;
-import boardgame.Tabuleiro;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
